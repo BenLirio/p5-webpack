0502c777ae14a66416996134e1b1b4d0efcaa8d1
Ben Lirio <benjlirio@gmail.com>
2023-02-03 17:52:27 -0500

    copyright

--- src/util/screenSize.ts
+++ src/util/screenSize.ts
@@ -1,5 +1,7 @@
 import p5 from 'p5'
 
+const WINDOW_PADDING = 20
+
 const aspect_16_9 = (width: number) => ({ width, height: width * 9 / 16 })
 
 export const getLargestSize = (p: p5) => [
@@ -13,10 +15,9 @@ export const getLargestSize = (p: p5) => [
     aspect_16_9(1366),
     aspect_16_9(1440),
     aspect_16_9(1600),
-  ].reduce((acc, size) => {
-    const { width, height } = size
-    if (p.windowWidth > width && p.windowHeight > height) {
-      return size
-    }
-    return acc
-  }, { width: 0, height: 0 })
+  ].reduce((acc, {width, height}) =>
+    p.windowWidth > width + WINDOW_PADDING
+    && p.windowHeight > height + WINDOW_PADDING
+    ? { width, height }
+    : acc
+  , { width: 0, height: 0 })
