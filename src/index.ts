6f0ecc576a1e9efc9879c81acca7aa36a97cb148
Ben Lirio <benjlirio@gmail.com>
2023-02-03 15:14:36 -0500

    move p5 into div

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import draw from './draw'
 import setup from './setup'
 
 window.addEventListener('DOMContentLoaded', () => {
+  const p5Div = document.getElementById('p5-div')
   new p5((p:p5) => {
     p.setup = () => setup(p)
     p.draw = () => draw(p)
-  })
+  }, p5Div)
 })
